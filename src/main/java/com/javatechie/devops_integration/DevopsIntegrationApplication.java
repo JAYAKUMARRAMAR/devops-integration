2e0fa1e01ca9b255806aedcc760a5e036f839cdc
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-07-25 23:47:14 +0530

    added getMapping

--- src/main/java/com/javatechie/devops_integration/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/devops_integration/DevopsIntegrationApplication.java
@@ -2,15 +2,17 @@ package com.javatechie.devops_integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-// @RestController
+@RestController
 public class DevopsIntegrationApplication {
 
-	// @GetMapping
-	// public String message(){
-	// 	return "welcome to javatechie";
-	// }
+	@GetMapping
+	public String message(){
+		return "welcome to javatechie";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsIntegrationApplication.class, args);
