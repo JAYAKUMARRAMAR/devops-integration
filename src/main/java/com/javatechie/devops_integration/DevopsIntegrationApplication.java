1d855bd32636dae0a578c3f6edd35b712b13fa07
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-07-25 23:01:08 +0530

    first commit

---
+++ src/main/java/com/javatechie/devops_integration/DevopsIntegrationApplication.java
@@ -0,0 +1,19 @@
+package com.javatechie.devops_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @RestController
+public class DevopsIntegrationApplication {
+
+	// @GetMapping
+	// public String message(){
+	// 	return "welcome to javatechie";
+	// }
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsIntegrationApplication.class, args);
+	}
+
+}
